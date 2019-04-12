514530a992e9b5fb41944bf79e2f12010f747a08
Filimon Danopoulos <filimon.danopoulos@gmail.com>
2019-04-13 00:38:05 +0200

    Updates

---
+++ precache-manifest.f617ac09ea743b886b7b162ad154b2e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d894a2df59a3b1415dde",
+    "url": "/sudoku/static/css/main.19d4947a.chunk.css"
+  },
+  {
+    "revision": "d894a2df59a3b1415dde",
+    "url": "/sudoku/static/js/main.7206c915.chunk.js"
+  },
+  {
+    "revision": "2376657f656b447c3d4b",
+    "url": "/sudoku/static/js/runtime~main.e7756cf6.js"
+  },
+  {
+    "revision": "06ec8c5cd670fd0e78bb",
+    "url": "/sudoku/static/js/2.ff715ced.chunk.js"
+  },
+  {
+    "revision": "b7e4e5f868085c6eda0239d39538fa94",
+    "url": "/sudoku/index.html"
+  }
+];
