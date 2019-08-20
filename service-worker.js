d5c50554535d67298c1a23642a627010db393cbb
Filimon Danopoulos <filimon.danopoulos@gmail.com>
2019-08-20 23:06:11 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sudoku/precache-manifest.0eac8e2d1f593b3940d44cacbd3bdc2c.js"
+  "/sudoku/precache-manifest.50452c0a0585acbcd42a5be2cd9e10da.js"
 );
 
 workbox.clientsClaim();
